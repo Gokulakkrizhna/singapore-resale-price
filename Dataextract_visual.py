51160bf1f142248141cd42de5f69f01d1923bc67
Gokulakkrizhna <166857603+Gokulakkrizhna@users.noreply.github.com>
2024-06-13 14:41:08 +0530

    Update Dataextract_visual.py

--- Dataextract_visual.py
+++ Dataextract_visual.py
@@ -1,5 +1,5 @@
 from Datacoll_dataclean import a,b,c,v1,v2,v3,v4,v5,v6,v7,v8,v9,V1,V2,V3,V4,V5,V6,V7,V8,V9
-from statistical_eda_analysis import s1,s2,e1,e2,e3,e4,e5,e6,e7,e8,e9
+from statistical_eda_analysis import s1,s2,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -9,29 +9,6 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import zipfile
 import os
-
-
-def load_model(zip_file):
-    # Directory where you want to extract the files
-    extract_dir = '/tmp/extracted_files/'
-
-    # Ensure extract_dir ends with the appropriate separator for the operating system
-    if not extract_dir.endswith(os.path.sep):
-        extract_dir += os.path.sep
-
-    try:
-        # Extract files from zip
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            zip_ref.extractall(extract_dir)
-
-        # Load the model from extracted pickle file
-        with open(os.path.join(extract_dir, 'pred.pkl'), 'rb') as file:
-            model = pickle.load(file)
-
-        return model
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
-        return None
     
 
 def streamlit():
@@ -50,11 +27,11 @@ def streamlit():
             b1 = v3.index(block)
             block = V3[b1]
             street = st.selectbox("Select the Street name",v4)
-            s1 = v4.index(street)
-            street = V4[s1]
+            S1 = v4.index(street)
+            street = V4[S1]
             storey = st.selectbox("Select the Storey range",v5)
-            s2 = v5.index(storey)
-            storey = V5[s2]
+            S2 = v5.index(storey)
+            storey = V5[S2]
         with col2:
             floor = st.number_input("Enter the floor area in sqm {0} and {1}".format(b[0],c[0]), value=b[0], placeholder="Type a number...")
             flat_ml = st.selectbox("Select the flat model",v6)
@@ -69,27 +46,196 @@ def streamlit():
             yrs_rm = st.selectbox("Select the years remaining",v9)
             y1 = v9.index(yrs_rm)
             yrs_rm = V9[y1]
+        
         if st.button("Predict Resale Price"):
-            zip_file = 'pred.zip'
-            model = load_model(zip_file)  # Load the model
-
-            if model:
-                # Perform prediction
-                try:
-                    y_pred = model.predict([[town, flat_ty, block, street, storey, floor, flat_ml, lease, trans_yr, yrs_rm]])
-                    pred_val = 10 ** (y_pred)
-                    st.subheader(f"Predicted Resale price: {pred_val[0]}")
-                except Exception as e:
-                    st.error(f"Error during prediction: {e}")
-            
-        # if st.button("Predict Resale Price"):
-        #         with open('pred.pkl', 'rb') as file:
-        #             model = pickle.load(file)
+                with open('ar.pkl', 'rb') as file:
+                    model = pickle.load(file)
                 
-        #         y_pred = model.predict([[town,flat_ty,block,street,storey,floor,flat_ml,lease,trans_yr,yrs_rm]])
-        #         pred_val = 10**(y_pred)
+                y_pred = model.predict([[town,flat_ty,block,street,storey,floor,flat_ml,lease,trans_yr,yrs_rm]])
+                pred_val = 10**(y_pred)
 
-        #         st.subheader(":red[Predicted Resale price:] {0}".format(pred_val[0]))
+                st.subheader(":red[Predicted Resale price:] {0}".format(pred_val[0]))
+    
+    with tab2:
+        st.header(":red[Correlation using Heatmap]")
+
+        fig = px.imshow(s1, text_auto=True, aspect="auto",color_continuous_scale="reds")
+        fig.update_layout(coloraxis_colorbar=dict(title="Correlation"))
+        st.plotly_chart(fig, use_container_width=True)
+
+        with st.expander("Do you like to see statistical data"):
+            st.dataframe(s2)
+
+    with tab3:
+        #Analysis1
+        st.header(":red[Analysis of Town over Resale Price]")
+        if e1 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different towns and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different towns and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis2
+        st.header(":red[Analysis of flat and resale price]")
+        if e2 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between flats and their resale price. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between flats and their resale price. It is a strong evidence.           
+            """)
+        
+        #Analysis3
+        st.header(":red[Analysis for block and resale price]")
+        if e3 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different blocks and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different blocks and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis4
+        st.header(":red[Analysis for Street Over price]")
+        if e4 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different street and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different street and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis5
+        st.header(":red[Analysis for resale price over storey range]")
+        if e5 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different storey range and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different storey range and their resale prices. It is a strong evidence.           
+            """)
+        
+        #Analysis6
+        st.header(":red[Analysis for Floor(sqm) over resale price]")
+        if e6 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different floor(sqm) and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different floor(sqm) and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis7
+        st.header(":red[Analysis for flat model over resale price]")
+        if e7 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between different flat model and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between different flat model and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis8
+        st.header(":red[Analysis for lease commence date over resale price]")
+        if e8 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection between lease commence dates and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection between lease commence dates and their resale prices. It is a strong evidence.           
+            """)
+
+        #Analysis9
+        st.header(":red[Analysis of Transaction year over resale price]")
+        if e9 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection over each transaction year and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection over each transaction year and their resale prices. It is a strong evidence.           
+            """)
+            
+        #Analysis10
+        st.header(":red[Analysis of years remaining over resale price]")
+        if e10 < 0.05:
+            st.markdown("""
+            - P-Value < 0.05
+            - Reject H0
+            - Dependent
+            - From this we can conclude that there is a significant connection over number of years remaining in the lease period and their resale prices. It is a strong evidence.           
+            """)
+        else:
+            st.markdown("""
+            - P-Value > 0.05
+            - Fail to Reject H0
+            - Independent
+            - From this we can conclude that there is no connection over number of years remaining in the lease period and their resale prices. It is a strong evidence.           
+            """)
 
 
 
